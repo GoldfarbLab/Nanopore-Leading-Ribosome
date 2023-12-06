769e3e0d37e2ce3bca3b381dbd4bcc41eb49a300
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-12-06 14:17:59 -0600

    Align reads

---
+++ RIS/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# setup paths
+BASE_PATH="/storage1/fs1/d.goldfarb/Active/Projects/Nanopore/"
+DATA_PATH=${BASE_PATH}/data
+REF_PATH=${BASE_PATH}/reference
+SRC_PATH=${BASE_PATH}/source
+
+SCRATCH_PATH="/scratch1/fs1/d.goldfarb/Nanopore/"
