b8cc4c0edd9d7c321452f5ad5b6a423edc8e0eeb
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-12-06 16:03:52 -0600

    convert sam to bam

--- RIS/config.sh
+++ RIS/config.sh
@@ -2,8 +2,14 @@
 
 # setup paths
 BASE_PATH="/storage1/fs1/d.goldfarb/Active/Projects/Nanopore/"
-DATA_PATH=${BASE_PATH}/data
 REF_PATH=${BASE_PATH}/reference
 SRC_PATH=${BASE_PATH}/source
-
 SCRATCH_PATH="/scratch1/fs1/d.goldfarb/Nanopore/"
+
+# reference files
+GENOME=${REF_PATH}/sacCer3.fa
+INTRONS=${REF_PATH}/introns.bed
+
+# parameters
+DATA_NAME="hel2ski2_polysomes"
+DATA_PATH=${BASE_PATH}/data/batch1/fastq_pass_hel2ski2_polysomes/*.gz
