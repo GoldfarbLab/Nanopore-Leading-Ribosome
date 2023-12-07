b7fa1675b2a5985674697f8c34dd3181b4bd9828
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-12-07 13:18:12 -0600

    Updated paths for cleaner output

--- RIS/config.sh
+++ RIS/config.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+# parameters
+DATA_NAME=hel2ski2_polysomes
+DATA_PATH=/storage1/fs1/d.goldfarb/Active/Projects/Nanopore//data/batch1/fastq_pass_hel2ski2_polysomes/*.gz
+
 # setup paths
-BASE_PATH="/storage1/fs1/d.goldfarb/Active/Projects/Nanopore/"
-REF_PATH=${BASE_PATH}/reference
-SRC_PATH=${BASE_PATH}/source
-SCRATCH_PATH="/scratch1/fs1/d.goldfarb/Nanopore/"
+PROJECT_PATH=/storage1/fs1/d.goldfarb/Active/Projects/Nanopore/
+REF_PATH=${PROJECT_PATH}/reference
+SRC_PATH=${PROJECT_PATH}/source
+SCRATCH_PATH=/scratch1/fs1/d.goldfarb/Nanopore/
+
+OUT_PATH=${SCRATCH_PATH}/${DATA_NAME}
+RESULTS_PATH=${OUT_PATH}/results
+LOG_PATH=${OUT_PATH}/logs
+SCRIPT_PATH=${OUT_PATH}/scripts
 
 # reference files
 GENOME=${REF_PATH}/sacCer3.fa
 INTRONS=${REF_PATH}/introns.bed
 CHROMSIZE=${REF_PATH}/chrom.sizes
 
-# parameters
-DATA_NAME="hel2ski2_polysomes"
-DATA_PATH=${BASE_PATH}/data/batch1/fastq_pass_hel2ski2_polysomes/*.gz
---
+++ RIS/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ./config.sh
+
+mkdir -p $OUT_PATH
+mkdir $RESULTS_PATH
+mkdir $LOG_PATH
+mkdir $SCRIPT_PATH
+
