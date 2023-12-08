e2eb0495ea5bb7387304fae3fa9ba8e70f5fa0be
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-12-08 13:07:11 -0600

    pipeline

--- RIS/config_template.sh
+++ RIS/config_template.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # parameters
-DATA_NAME=hel2ski2_polysomes
-DATA_PATH=/storage1/fs1/d.goldfarb/Active/Projects/Nanopore//data/batch1/fastq_pass_hel2ski2_polysomes/*.gz
+DATA_NAME=
+DATA_PATH=
+
+# LSF parameters
+LSF_g=/d.goldfarb/compute
+LSF_G=compute-d.goldfarb
 
 # setup paths
 PROJECT_PATH=/storage1/fs1/d.goldfarb/Active/Projects/Nanopore/
@@ -10,6 +14,7 @@ REF_PATH=${PROJECT_PATH}/reference
 SRC_PATH=${PROJECT_PATH}/source
 SCRATCH_PATH=/scratch1/fs1/d.goldfarb/Nanopore/
 
+# output paths
 OUT_PATH=${SCRATCH_PATH}/${DATA_NAME}
 RESULTS_PATH=${OUT_PATH}/results
 LOG_PATH=${OUT_PATH}/logs
--- RIS/prepare.sh
+++ RIS/prepare.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+# Read command line arguments
+DATA_NAME=$1
+DATA_PATH=$2
+
+# Create config file from template and command line arguments
+sed "s+DATA_NAME=+DATA_NAME=$DATA_NAME+g" < config_template.sh > config.sh
+sed -i "s+DATA_PATH=+DATA_PATH=$DATA_PATH/*.gz+g" config.sh
+
+# Read config
 source ./config.sh
 
+# Create output folders
 mkdir -p $OUT_PATH
 mkdir $RESULTS_PATH
 mkdir $LOG_PATH
 mkdir $SCRIPT_PATH
+
+# Update scripts with dataset specific parameters
+declare -a scripts=("align.bsub" "bam.bsub" "bedgraph.bsub" "bigwig.bsub")
+
+for val in ${scripts[@]}; do
+    sed "s+LSF_g+$LSF_g+g" < $val > $SCRIPT_PATH/$val
+    sed -i "s+LSF_G+$LSF_G+g" $SCRIPT_PATH/$val
+    sed -i "s+LSF_LOG_PATH+$LOG_PATH/+g" $SCRIPT_PATH/$val
+    sed -i "s+LSF_SCRIPT_PATH+$SCRIPT_PATH/+g" $SCRIPT_PATH/$val
+done
+
+
+
 
